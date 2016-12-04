b39708084c523ffaa797df77f3e4939ae0614dc5
Jeff Doll <jeff.doll@workiva.com>
2016-12-04 17:17:28 -0600

    mod response for parsing voltsec

--- index.js
+++ index.js
@@ -7,12 +7,15 @@ module.exports = function(homebridge) {
 	Characteristic = homebridge.hap.Characteristic;
 
 	homebridge.registerAccessory("homebridge-httpdoor", "Httpdoor", DoorAccessory);
-}
+};
 
 function DoorAccessory(log, config) {
 	this.log = log;
 	this.name = config["name"];
-	this.controlurl = config["controlURL"];
+	this.deviceID = config["deviceID"];
+	this.openState = config["openState"];
+	this.closedState = config["closedState"];
+	this.controlURL = config["controlURL"];
 	this.statusurl = config["statusURL"];
 
 	this.garageservice = new Service.GarageDoorOpener(this.name);
@@ -25,10 +28,32 @@ function DoorAccessory(log, config) {
 		.getCharacteristic(Characteristic.TargetDoorState)
 		.on('get', this.getState.bind(this))
 		.on('set', this.setState.bind(this));
+}
 
-	this.garageservice
-		.getCharacteristic(Characteristic.ObstructionDetected)
-		.on('get', this.getState.bind(this));
+function parseStateResponse(body, deviceID)
+{
+	var doorState = null;
+	var x = body.split('\n')[4];
+	var statuses = x.substring(0, x.length - 9).split(",");
+	var l = statuses.length;
+	for (var i = 0; i < l; i++)
+	{
+		if (statuses[i].indexOf(deviceID) >= 0)
+		{
+			var state = statuses[i].split("|")[1];
+			if (state == "0")
+			{
+				doorState = "closed";
+			}
+			else if (state == "1")
+			{
+				doorState = "open";
+			} else {
+				doorState = "moving";
+			}
+		}
+	}
+	return doorState;
 }
 
 DoorAccessory.prototype.getState = function(callback) {
@@ -38,29 +63,25 @@ DoorAccessory.prototype.getState = function(callback) {
 		url: this.statusurl
 	}, function(err, response, body) {
 		if (!err && response.statusCode == 200) {
-			var json = JSON.parse(body);
-			var state = json.state; // "open" or "closed"
-			this.log("Door state is %s", state);
-			var closed = state == "closed"
+			var pollState = parseStateResponse(body, this.deviceID);
+			var closed = pollState == "closed";
 			callback(null, closed); // success
 		} else {
 			this.log("Error getting state: %s", err);
 			callback(err);
 		}
 	}.bind(this));
-}
+};
 
 DoorAccessory.prototype.setState = function(state, callback) {
 	var doorState = (state == Characteristic.TargetDoorState.CLOSED) ? "closed" : "open";
 	this.log("Set state to %s", doorState);
-	
 	request.get({
 		url: this.controlurl
 	}, function(err, response, body) {
 		if (!err && response.statusCode == 200) {
 			this.log("State change complete.");
 			var currentState = (state == Characteristic.TargetDoorState.CLOSED) ? Characteristic.CurrentDoorState.CLOSED : Characteristic.CurrentDoorState.OPEN;
-
 			this.garageservice
 			.setCharacteristic(Characteristic.CurrentDoorState, currentState);
 
@@ -70,8 +91,8 @@ DoorAccessory.prototype.setState = function(state, callback) {
 			callback(err || new Error("Error setting door state."));
 		}
 	}.bind(this));
-},
+};
 
 DoorAccessory.prototype.getServices = function() {
 	return [this.garageservice];
-}
+};
